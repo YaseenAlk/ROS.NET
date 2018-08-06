72e69249af08a20e4b43741f8a8435852f309421
YaseenAlk <alkhafaji.yaseen@gmail.com>
2018-08-06 18:15:30 -0400

    YAMLParser: (experimentation) Allow strings to be constants

--- YAMLParser/SingleType.cs
+++ YAMLParser/SingleType.cs
@@ -158,14 +158,16 @@ namespace FauxMessages
                 string prefix = "", suffix = "";
                 if (isconst)
                 {
-                    if (!type.Equals("string", StringComparison.OrdinalIgnoreCase))
-                    {
+                    // why can't strings be constants?
+
+                    //if (!type.Equals("string", StringComparison.OrdinalIgnoreCase))
+                    //{
                         if (KnownStuff.IsPrimitiveType(this))
                             prefix = "const ";
                         else
                             prefix = "static readonly ";
                         wantsconstructor = false;
-                    }
+                    //}
                 }
 
                 string t = KnownStuff.GetNamespacedType(this, type);
@@ -283,10 +285,12 @@ namespace FauxMessages
                 string prefix = "", suffix = "";
                 if (isconst)
                 {
-                    if (!Type.Equals("string", StringComparison.OrdinalIgnoreCase))
-                    {
+                    // why can't strings be constants?
+
+                    //if (!Type.Equals("string", StringComparison.OrdinalIgnoreCase))
+                    //{
                         prefix = "const ";
-                    }
+                    //}
                 }
                 if (otherstuff.Contains('='))
                     if (wantsconstructor)
